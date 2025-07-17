f78f419d68a0ece4e09ba7552b3ead12874227ad
Antonios Saliba <168730127+antoniossaliba@users.noreply.github.com>
2025-07-17 10:53:09 +0300

    GET request to /review/:isbn to fetch the reviews of a specific book based on its isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -93,8 +93,19 @@ public_users.get('/title/:title', (req, res) => {
 
 //  Get book review
 public_users.get('/review/:isbn', (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+
+    res.send(JSON.stringify(books[isbn].reviews, null, 5));
+
+  } else {
+
+    res.status(404).json({ message: "ISBN not valid" });
+
+  }
+
 });
 
 module.exports.general = public_users;
