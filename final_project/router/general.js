59858f559fbf206f17e1679cda95af7bfc77d05f
Antonios Saliba <168730127+antoniossaliba@users.noreply.github.com>
2025-07-17 14:29:07 +0300

    Tasks 10-11-12-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,31 +47,31 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', (req, res) => {
+public_users.get('/', async (req, res) => {
 
-  res.send(JSON.stringify(books, null, 5));
+  await res.send(JSON.stringify(books, null, 5));
 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
+public_users.get('/isbn/:isbn', async (req, res) => {
 
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
 
-    res.send(JSON.stringify(books[isbn], null, 5));
+    await res.send(JSON.stringify(books[isbn], null, 5));
 
   } else {
 
-    res.status(404).json({ message: "ISBN not valid!" });
+    await res.status(404).json({ message: "ISBN not valid!" });
 
   }
 
 });
   
 // Get book details based on author
-public_users.get('/author/:author', (req, res) => {
+public_users.get('/author/:author', async (req, res) => {
 
   const author = req.params.author;
 
@@ -81,26 +81,26 @@ public_users.get('/author/:author', (req, res) => {
 
       if (value.author === author) {
 
-        res.send(JSON.stringify(value, null, 5));
+        await res.send(JSON.stringify(value, null, 5));
         return;
 
       }
 
     }
 
-    res.send(JSON.stringify({ message: `${author} not present` }, null, 5));
+    await res.send(JSON.stringify({ message: `${author} not present` }, null, 5));
     return;
 
   } else {
 
-    res.status(404).json({ message: "Author not provided!" })
+    await res.status(404).json({ message: "Author not provided!" })
 
   }
 
 });
 
 // Get all books based on title
-public_users.get('/title/:title', (req, res) => {
+public_users.get('/title/:title', async (req, res) => {
 
   const title = req.params.title;
 
@@ -110,35 +110,35 @@ public_users.get('/title/:title', (req, res) => {
 
       if (value.title === title) {
 
-        res.send(JSON.stringify(value, null, 5));
+        await res.send(JSON.stringify(value, null, 5));
         return;
 
       }
 
     }
 
-    res.status(404).json({ message: `${title} not found!` });
+    await res.status(404).json({ message: `${title} not found!` });
 
   } else {
 
-    res.status(404).json({ message: "Title not provided!" });
+    await res.status(404).json({ message: "Title not provided!" });
 
   }
 
 });
 
 //  Get book review
-public_users.get('/review/:isbn', (req, res) => {
+public_users.get('/review/:isbn', async (req, res) => {
 
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
 
-    res.send(JSON.stringify(books[isbn].reviews, null, 5));
+    await res.send(JSON.stringify(books[isbn].reviews, null, 5));
 
   } else {
 
-    res.status(404).json({ message: "ISBN not valid" });
+    await res.status(404).json({ message: "ISBN not valid" });
 
   }
 
